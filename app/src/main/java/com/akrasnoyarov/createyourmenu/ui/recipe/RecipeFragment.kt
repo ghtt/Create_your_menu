138d073abd255794a3d1c03e5df6bdffdc4fac56
akrasnoyaro2 <Artem.Krasnoyarov2@harman.com>
2021-03-17 17:27:27 +0300

    Added a recipe layout

--- app/src/main/java/com/akrasnoyarov/createyourmenu/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/akrasnoyarov/createyourmenu/ui/recipe/RecipeFragment.kt
@@ -1,18 +1,28 @@
-package com.akrasnoyarov.createyourmenu.ui.viewmodel.recipe
+package com.akrasnoyarov.createyourmenu.ui.recipe
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.akrasnoyarov.createyourmenu.databinding.FragmentRecipeBinding
 
 class RecipeFragment : Fragment() {
+    private var _binding: FragmentRecipeBinding? = null
+    private val binding: FragmentRecipeBinding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
