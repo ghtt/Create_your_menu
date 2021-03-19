daae1cc9503c0cea223956c67649ab4d1cf1a10c
akrasnoyaro2 <Artem.Krasnoyarov2@harman.com>
2021-03-19 17:10:14 +0300

    Added recipe by id api call

--- app/src/main/java/com/akrasnoyarov/createyourmenu/ui/MainActivity.kt
+++ app/src/main/java/com/akrasnoyarov/createyourmenu/ui/MainActivity.kt
@@ -3,11 +3,14 @@ package com.akrasnoyarov.createyourmenu.ui
 import androidx.fragment.app.Fragment
 import com.akrasnoyarov.createyourmenu.ui.categories.CategoriesFragment
 import com.akrasnoyarov.createyourmenu.ui.recipe.RecipeFragment
+import com.akrasnoyarov.createyourmenu.ui.recipe.RecipesListFragment
 
 class MainActivity : SingleFragmentActivity() {
 
     override fun createFragment(): Fragment {
 //        return RecipeFragment.newInstance()
-        return CategoriesFragment.newInstance()
+//        return CategoriesFragment.newInstance()
+        return RecipesListFragment.newInstance("Chicken")
     }
+
 }
